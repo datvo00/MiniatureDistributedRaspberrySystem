e3919dd900d08b544ef1ccabf653f6c4daf750d7
dat <dat.vo.primary@gmail.com>
2023-03-12 08:29:13 -0700

    fixed minor pico bugs

--- Pi-Code/comm.py
+++ Pi-Code/comm.py
@@ -10,7 +10,7 @@ def DeviceID(device):
     poll.register(device_serial, select.POLLIN)
     comm_attempt = 1
     while comm_attempt <= 3:
-        device_serial.write("id \n") # makes id request
+        device_serial.write("id \n".encode()) # makes id request
         events = poll.poll(500) # waits 0.5 second for response
         for fd, status in events:
             if status & select.POLLIN:
@@ -18,15 +18,16 @@ def DeviceID(device):
                 device_id = device_id.decode("utf-8", "ignore")
                 return device_id
         comm_attempt = comm_attempt + 1
-    return -1
+    return ""
 
 
 def ConnectedDevices():
     device_list = {}
     devices_path = glob.glob(DEVICE_PATH_PI)
     for device in devices_path:
+        print(device)
         device_id = DeviceID(device)
-        if device_id >= 0:
+        if len(device_id) > 0:
             device_list[device_id] = device
     return device_list
     
--- Pi-Code/main.py
+++ Pi-Code/main.py
@@ -2,6 +2,6 @@ from comm import ConnectedDevices
 
 command = ""
 
-while command != "exit":
-    connected_devices = ConnectedDevices()
-    print(connected_devices)
+#while command != "exit":
+connected_devices = ConnectedDevices()
+print(connected_devices)
